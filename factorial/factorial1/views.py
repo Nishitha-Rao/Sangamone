2927672a069c289b7297f9bb40a77e042e45773e
Nishitha B Rao <149511926+Nishitha-Rao@users.noreply.github.com>
2023-11-30 23:05:31 +0530

    Add files via upload
    first commit

---
+++ factorial/factorial1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .factorial import find_factorial as ff
+
+def home(request):
+    factorial = ff()
+    return render(request,'index.html',{'param1':factorial})
+
+
+# Create your views here.
